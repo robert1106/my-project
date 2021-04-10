44c3cff61dad2f7e4eca12ecb82242e93c0369f0
robert1106 <robota1106@gmail.com>
2021-04-10 22:33:04 +0300

    add new project

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import {HttpClientModule} from "@angular/common/http";
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import {FormsModule} from "@angular/forms";
+import {TodosFilterPipe} from "./shared/todos-filter.pipe";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoFormComponent,
+    TodosFilterPipe
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
