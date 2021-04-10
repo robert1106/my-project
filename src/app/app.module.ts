3be25e1ad81d1929747d3267b1bf172a31d67cdd
robert1106 <robota1106@gmail.com>
2021-04-10 23:32:23 +0300

    add all project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
+
+
+
+
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
 import {HttpClientModule} from "@angular/common/http";
